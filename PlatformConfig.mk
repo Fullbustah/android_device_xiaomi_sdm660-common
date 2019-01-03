d55735b93584da15a8b7192d7546a8044b47f71d
Fullbustah <adam.melicharek@gmail.com>
2019-01-03 09:40:09 +0100

    allow sepolicy

--- PlatformConfig.mk
+++ PlatformConfig.mk
@@ -279,9 +279,9 @@ ENABLE_VENDOR_RIL_SERVICE := true
 BOARD_SECCOMP_POLICY := $(PLATFORM_PATH)/seccomp
 
 # SELinux
-#include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
-#SELINUX_IGNORE_NEVERALLOWS := true
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Timeservice
 BOARD_USES_QC_TIME_SERVICES := true
